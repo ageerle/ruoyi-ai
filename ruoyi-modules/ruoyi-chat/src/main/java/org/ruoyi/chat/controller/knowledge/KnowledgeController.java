52e0feda011e4af5e294aeb2afd10ec796234d81
zhouweiyi <>
2025-05-13 13:44:31 +0800

    修改方法描述

--- ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/chat/controller/knowledge/KnowledgeController.java
+++ ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/chat/controller/knowledge/KnowledgeController.java
@@ -168,7 +168,7 @@ public class KnowledgeController extends BaseController {
    * 提取PDF中的图片并调用gpt-4o-mini,识别图片内容并返回
    *
    * @param file PDF文件
-   * @return 保存的文件路径信息
+   * @return 文件名称和图片内容
    */
   @PostMapping("/extract-images")
   @Operation(summary = "提取PDF中的图片并调用大模型,识别图片内容并返回", description = "提取PDF中的图片并调用gpt-4o-mini,识别图片内容并返回")
