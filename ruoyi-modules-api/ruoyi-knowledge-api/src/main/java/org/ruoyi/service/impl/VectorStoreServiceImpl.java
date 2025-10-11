77f7ac0af1e086e9ffce3e25476c96920923c72d
Yzm <yanziming022@gmail.com>
2025-10-11 20:09:15 +0800

    refactor(knowledge): 标记向量存储服务为首选实现
    - 添加 @Primary 注解以指定为主要 Bean 实现
    - 确保在多个实现存在时优先使用该服务

--- ruoyi-modules-api/ruoyi-knowledge-api/src/main/java/org/ruoyi/service/impl/VectorStoreServiceImpl.java
+++ ruoyi-modules-api/ruoyi-knowledge-api/src/main/java/org/ruoyi/service/impl/VectorStoreServiceImpl.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
  * @author ageer
  */
 @Service
+@Primary
 @Slf4j
 @RequiredArgsConstructor
 public class VectorStoreServiceImpl implements VectorStoreService {
