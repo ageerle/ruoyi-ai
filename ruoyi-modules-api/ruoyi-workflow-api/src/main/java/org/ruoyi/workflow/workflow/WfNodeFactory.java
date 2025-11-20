8a1ac2264e5b38bd614d31faba9a34fb4301368b
stageluo <979175267@qq.com>
2025-11-20 10:09:48 +0800

    优化工作流注册节点以及相关其他代码优化

--- ruoyi-modules-api/ruoyi-workflow-api/src/main/java/org/ruoyi/workflow/workflow/WfNodeFactory.java
+++ ruoyi-modules-api/ruoyi-workflow-api/src/main/java/org/ruoyi/workflow/workflow/WfNodeFactory.java
@@ -5,7 +5,10 @@ import org.ruoyi.workflow.entity.WorkflowNode;
 import org.ruoyi.workflow.workflow.node.AbstractWfNode;
 import org.ruoyi.workflow.workflow.node.EndNode;
 import org.ruoyi.workflow.workflow.node.answer.LLMAnswerNode;
+import org.ruoyi.workflow.workflow.node.keywordExtractor.KeywordExtractorNode;
+import org.ruoyi.workflow.workflow.node.mailSend.MailSendNode;
 import org.ruoyi.workflow.workflow.node.start.StartNode;
+import org.ruoyi.workflow.workflow.node.switcher.SwitcherNode;
 
 public class WfNodeFactory {
     public static AbstractWfNode create(WorkflowComponent wfComponent, WorkflowNode nodeDefinition,
@@ -14,7 +17,10 @@ public class WfNodeFactory {
         switch (WfComponentNameEnum.getByName(wfComponent.getName())) {
             case START -> wfNode = new StartNode(wfComponent, nodeDefinition, wfState, nodeState);
             case LLM_ANSWER -> wfNode = new LLMAnswerNode(wfComponent, nodeDefinition, wfState, nodeState);
+            case KEYWORD_EXTRACTOR -> wfNode = new KeywordExtractorNode(wfComponent, nodeDefinition, wfState, nodeState);
             case END -> wfNode = new EndNode(wfComponent, nodeDefinition, wfState, nodeState);
+            case MAIL_SEND -> wfNode = new MailSendNode(wfComponent, nodeDefinition, wfState, nodeState);
+            case SWITCHER -> wfNode = new SwitcherNode(wfComponent, nodeDefinition, wfState, nodeState);
             default -> {
             }
         }
